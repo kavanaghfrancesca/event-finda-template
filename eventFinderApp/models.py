0cdea019409dd19181dd96028a40ddef0281f47c
Francesca <kavanaghfrancesca@gmail.com>
2019-09-14 11:03:17 +0800

    Working website Week 9 before beautifying

--- eventFinderApp/models.py
+++ eventFinderApp/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
-
 class Event(models.Model):
     title = models.CharField(max_length=200)
+    venue = models.CharField(max_length=200,null=True)
     location = models.CharField(max_length=200)
     start_time = models.DateTimeField('start time and date')
     end_time = models.DateTimeField('end time and date')
+    categories = models.ManyToManyField('Category', related_name='events')
+    def __str__(self):
+        return self.title    
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,null=True)
+    def __str__(self):
+        return self.name
--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -31,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'eventFinderApp.apps.EventfinderappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = '/event-finder'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'eventFinderProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],            # <------- THIS LINE HAS BEEN UPDATED
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +73,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'eventFinderProject.wsgi.application'
 
 
@@ -77,7 +83,7 @@ WSGI_APPLICATION = 'eventFinderProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'freeeventfinder', # you will need to create this db
+        'NAME': 'event_finder', # you will need to create this db
         'USER': '', # enter your user name here
         'PASSWORD': '',
         'HOST': 'localhost',
@@ -122,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
